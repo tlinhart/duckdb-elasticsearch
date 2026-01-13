db25abe93e25ce1b56d6085bf0f163eb66acbea3
Tomáš Linhart <pasmen@gmail.com>
2026-01-13 20:44:10 +0100

    Add build config files

---
+++ extension_config.cmake
@@ -0,0 +1,11 @@
+# This file is included by DuckDB's build system. It specifies which extensions
+# to load.
+
+# Extension from this repo.
+duckdb_extension_load(elasticsearch
+    SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}
+    LOAD_TESTS
+)
+
+# Any extra extensions that should be built.
+# duckdb_extension_load(json)
